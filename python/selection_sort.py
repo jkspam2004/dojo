c72a63d583df8e8c47d399c86111889cfc522e50
jkspam2004 <jkspam2004@yahoo.com>
2016-12-24 14:06:26 -0800

    add more context for selection sort

--- python/selection_sort.py
+++ python/selection_sort.py
@@ -3,5 +3,35 @@
 # 2. find the minimum value
 # 3. swap it to the beginning of unsorted sublist
 
+from random import randint
+list = [] 
+print list
+
+def get_list():
+    for num in range(5):
+        print randint(0,10001)
+        list.append(randint(0,10001)) 
+
+    return list
+
+def selection_sort(arr):
+    for k in range(len(arr)):
+        min_idx = k
+        for i in range(k, len(arr)):
+            if arr[i] < arr[min_idx]:
+                min_idx = i
+        swap(arr, min_idx, k)
+
+    return arr
+
+def swap(arr, first, second):
+    temp = arr[first]
+    arr[first] = arr[second]
+    arr[second] = temp
+    return arr
+
+
+list = get_list()
+print selection_sort(list)
 
 
